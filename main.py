7de8b7496b14ddce953ac8e90abe74355bf6c42d
antoniorobotica76 <80981840+antoniorobotica76@users.noreply.github.com>
2021-03-19 19:46:57 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,10 @@
+basic.show_icon(IconNames.HEART)
+
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.pause(500)
+    basic.show_icon(IconNames.HAPPY)
+    basic.pause(500)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+})
